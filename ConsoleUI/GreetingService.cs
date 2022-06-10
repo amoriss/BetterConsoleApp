633c68a81daf6447c07ffdfcd10c0049399229c2
Amoriss <amoriss@protonmail.com>
2022-06-09 23:21:04 -0500

    Dependency Injection

---
+++ ConsoleUI/GreetingService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+// Goals: DI, Serilog, Settings
+
+namespace ConsoleUI
+{
+    partial class Program
+    {
+        public class GreetingService : IGreetingService
+        {
+            private readonly ILogger<GreetingService> _log;
+            private readonly IConfiguration _config;
+
+            public GreetingService(ILogger<GreetingService> log, IConfiguration config)
+            {
+                _log = log;
+                _config = config;
+            }
+
+            public void Run()
+            {
+                for (int i = 0; i < _config.GetValue<int>("LoopTimes"); i++)
+                {
+                    // use composite formatting instead of string interpolation
+                    // because in logging if you have a structure logger
+                    // then Serilog will not only log the text, but log the value separately
+                    // so you can query logs based on their numbers and you don't have to parse strings
+                    _log.LogInformation("Run number {runNumber}", i);
+                }
+            }
+        }
+
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Serilog;
 using System;
 using System.IO;
@@ -10,7 +9,7 @@ using System.IO;
 
 namespace ConsoleUI
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
@@ -30,7 +29,7 @@ namespace ConsoleUI
             var host = Host.CreateDefaultBuilder()
                 .ConfigureServices((context, services) =>
                 {
-                    
+                    services.AddTransient<GreetingService>();
                 })
                 .UseSerilog()
                 .Build();
@@ -41,30 +40,6 @@ namespace ConsoleUI
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
                 .AddEnvironmentVariables();
-        }
-
-        public class GreetingService
-        {
-            private readonly ILogger<GreetingService> _log;
-            private readonly IConfiguration _config;
-
-            public GreetingService(ILogger<GreetingService> log, IConfiguration config)
-            {
-                _log = log;
-                _config = config;
-            }
-
-            public void Run()
-            {
-                for (int i = 0; i < _config.GetValue<int>("LoopTimes"); i++)
-                {
-                    // use composite formatting instead of string interpolation
-                    // because in logging if you have a structure logger
-                    // then Serilog will not only log the text, but log the value separately
-                    // so you can query logs based on their numbers and you don't have to parse strings
-                    _log.LogInformation("Run number {runNumber}", i);
-                }
-            }
         }
 
     }
